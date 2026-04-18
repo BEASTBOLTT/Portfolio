aef985a6435675e686df9fa60754c0cf6b687b9f
Devam <devamdp27@gmail.com>
2026-04-19 01:51:28 +0530

    Header issue

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
 
   return (
     <div className="h-screen w-screen flex flex-col">
-      <div className=" bg-[#323233] text-white p-3 w-screen flex">
+      <div className=" bg-[#323233] text-white p-3 w-full flex">
         <svg onClick={handleMenu} className="h-5 w-5 mr-2 mt-1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="#ADADAD" stroke="currentColor" d="m2.75 12.25h10.5m-10.5-4h10.5m-10.5-4h10.5" /></svg>
         <h1 className="mt-0.5 text-[#ADADAD] font-bold">Portfolio - Devam Pandey</h1>
       </div>
